f2ffbbdfadb17df7879b23424cf7b31b2153096c
AJAY SURESH SJ <93826214+Ajay-2004@users.noreply.github.com>
2022-03-11 19:48:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from pyrogram import Client, filters
 Mikasa=Client(
   "File Store Bot",
   bot_token='5270572716:AAGYa0MpE948B9SL_Fz5aK0zxKhW-DiEGbQ',
-  api_id='9969145',
-  api_hash='96940e396888e9b5a2d7cf57757fc1a6 '
+  api_id='14562867',
+  api_hash='c54613404f9bcdbd0c90171e346de396'
 )
 
 @Mikasa.on_message(filters.command("Start"))
